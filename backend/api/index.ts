cfbd2f20a026f39d7b43ef3c34fd36cbdb1ce85a
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-05-05 15:35:40 -0700

    Remove db import

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,6 +1,5 @@
 import { handle } from "hono/vercel"
 import { type Context, Hono } from "hono"
-import { getEvents, seed } from "@/api/db"
 import type { BlankEnv, BlankInput } from "hono/types"
 
 const app = new Hono()
@@ -9,15 +8,15 @@ app.get("/", (c: Context<BlankEnv, "/", BlankInput>) => {
   return c.json({ message: "Hello Hono!" })
 })
 
-app.get("/events", async (c: Context<BlankEnv, "/", BlankInput>) => {
-  const events = await getEvents()
-  return c.json({ events })
-})
+// app.get("/events", async (c: Context<BlankEnv, "/", BlankInput>) => {
+//   const events = await getEvents()
+//   return c.json({ events })
+// })
 
-app.get("/events/seed", async (c: Context<BlankEnv, "/", BlankInput>) => {
-  await seed()
-  return c.json({ message: "Seeded events" })
-})
+// app.get("/events/seed", async (c: Context<BlankEnv, "/", BlankInput>) => {
+//   await seed()
+//   return c.json({ message: "Seeded events" })
+// })
 
 export default {
   port: 8081,
